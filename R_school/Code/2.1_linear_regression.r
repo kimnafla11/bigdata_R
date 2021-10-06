b0215e6897071b61537073900c22528f2df1a8bf
김나은 <50188040+kimnafla11@users.noreply.github.com>
2021-10-06 23:03:54 +0900

    Add files via upload

--- R_school/Code/2.1_linear_regression.r
+++ R_school/Code/2.1_linear_regression.r
@@ -35,4 +35,32 @@ model$coefficients
 
 model$fitted.values
 #예측값(y햇) 
-plot(model$fitted.values)
+plot(df$wt,model$fitted.values)
+
+summary(model)
+#summary결과표에 별표따라 베타 값을 너무 신뢰해서는 안됨
+#x끼리 상관관계가 너무 높을때 어떤 y를 설명할때 신뢰하기 어려운현상인 다중공선성 문제 발생 가능
+#R2가 높으면 F통계량도 높은경향이 있다
+
+
+####선형대수학으로 linear regression 해보기####
+typeof(df)
+class(df) #데이터프레임은 선형대수로 표현하기 어려움
+
+x = as.matrix(df[,1:5])
+class(x)
+y = as.vector(df[,6])
+class(y)
+
+x = cbind(matrix(1,nrow=nrow(x)),x) #x매트릭스에 1을 추가함
+y
+
+#solve() 역행렬 함수
+hat = solve(t(x) %*% x)%*%(t(x)%*%y)
+print(hat)
+
+model$coefficients
+#우와 존나 신기하다.
+yhat= x %*% hat
+
+plot(model$fitted.values, yhat)
